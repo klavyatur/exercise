17de8d05c0bde171a56eaf65341151169ca92e9f
k <klavyatur@yahoo.com>
2021-03-24 18:04:50 -0400

    some api fields only returning 'null' -- had to pivot

--- client/components/DisplayData.js
+++ client/components/DisplayData.js
@@ -3,16 +3,59 @@ import API from '../reactAPI';
 
 export default function DisplayDataR() {
 
+  const [ zip, setZip ] = useState(12345);
+  const [ dist, setDist ] = useState(10)
+  const [ submitClicked, setSubmitClicked ] = useState(false)
+  const [ schoolList, setSchoolList ] = useState(<></>);
+
     useEffect(() => {
-    API.getAPIResponse()
+    API.getAPIResponse(zip, dist)
     .then((res) => {
       console.log(res.data.results);
+      let schools = res.data.results;
+      let list = schools.map((el) => {
+       return <div key={el.id}>
+          <div>Name: <a href={el['school.school_url']}>{el['school.name']}</a></div>
+          <div>Location: {el['school.city']}, {el['school.state']}</div>
+        </div>
+      });
+      console.log(list);
+      setSchoolList(list);
     })
     .catch(err => console.log('getDetails: ERROR: ', err));
-  }, [])
+  }, [submitClicked])
  
+  function buttonClick(e) {
+    e.preventDefault();
+    setSubmitClicked(!submitClicked)
+  }
 
   return (
-    <div>Welcome to </div>
+    <div>
+      <div>
+        <form style={{ display: 'flex' }}>
+          Find schools near 
+          <input 
+            type='tezt' 
+            defaultValue='12345'
+            onChange={(e) => setZip(e.target.value)}
+            style={{ padding: '5px', margin: '10px' }}
+          /> 
+          zipcode (within 
+          <input 
+            type='text' 
+            defaultValue='10'
+            onChange={(e) => setDist(e.target.value)}
+            style={{ padding: '5px', margin: '10px' }}
+          /> 
+          miles). 
+          <button type='button' onClick={buttonClick} style={{ padding: '5px', margin: '10px' }}>Search</button>
+        </form>
+      </div>
+      <div>
+        <h3>Schools:</h3>
+        {schoolList}
+      </div>
+    </div>
   )
 }
--- client/reactAPI.js
+++ client/reactAPI.js
@@ -1,9 +1,12 @@
 import axios from 'axios';
-const COLLEGE_SCORECARD_URL = 'https://api.data.gov/ed/collegescorecard/v1/schools?api_key=JV2IyZTwqh1cEjHq3B0WKctKfFUeC509iHybf7pU';
-
 
 export default {
-    getAPIResponse() {
+    getAPIResponse(zip, dist) {
+        const COLLEGE_SCORECARD_URL = `https://api.data.gov/ed/collegescorecard/v1/schools?per_page=100&zip=${zip}&distance=${dist}mi&fields=id,school.name,school.state,school.city,school.school_url&api_key=JV2IyZTwqh1cEjHq3B0WKctKfFUeC509iHybf7pU`;
+
         return axios.get(COLLEGE_SCORECARD_URL);
     }
 };
+
+
+
